af61fc89d36a094e3cdb646beb0640360560ae21
mokesh <mokeshbalasubramani985@gmail.com>
2024-09-01 17:40:14 -0100

    final changes

--- resources/views/admin/footer.blade.php
+++ resources/views/admin/footer.blade.php
@@ -2,6 +2,6 @@
 <footer class="footer">
 <div class="d-sm-flex justify-content-center justify-content-sm-between">
     <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © Mokesh B 2024</span>
-</div>
+</div> 
 </footer>
 <!-- footer ends -->
